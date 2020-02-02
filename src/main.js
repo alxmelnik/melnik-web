bd0308d6b72c3c4cce7158cd0bdbecb10ec73708
Alexsandr <alxmelnik@yandex.ru>
2020-02-02 21:59:07 +0300

    2й слайдер

--- src/main.js
+++ src/main.js
@@ -6,4 +6,5 @@ if (process.env.NODE_ENV === "development") {
 import "./scripts/welcome-parallax";
 import "./scripts/fullscreen-menu";
 import "./scripts/skills";
+import "./scripts/works";
 import "./scripts/reviews";
